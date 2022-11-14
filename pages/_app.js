df4cb0db36a8fbe5d383ab6a5a8ac2cc91ae5d91
Jason Wilson <wilson@Jasons-Air.home>
2022-11-13 22:58:55 -0600

    added ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+//...
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=XXXXXXXXX`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-8P3NH9Z5VP', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+   <Component {...pageProps} />
+   </>
+  );
 }
 
 export default MyApp
